ab1c6af5a5e45c4b2d01f815581f1537463e5065
KhinYadanarNg <e0015793@u.nus.edu>
2025-08-08 00:18:37 +0800

    added authenticationManager

--- src/main/java/sg/edu/nus/iss/edgp/workflow/management/configuration/EDGPWorkflowManagementSecurityConfig.java
+++ src/main/java/sg/edu/nus/iss/edgp/workflow/management/configuration/EDGPWorkflowManagementSecurityConfig.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -72,6 +74,11 @@ public class EDGPWorkflowManagementSecurityConfig {
 						.jwt(jwt -> jwt.jwtAuthenticationConverter(jwtAuthenticationConverter()))
 					)
 				.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class).build();
+	}
+	
+	@Bean
+	public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception {
+		return authConfig.getAuthenticationManager();
 	}
 
 }
