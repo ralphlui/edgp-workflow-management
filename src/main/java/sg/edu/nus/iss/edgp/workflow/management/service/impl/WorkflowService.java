8d7efe32ed1f1f4c9bc434cf95cc4ad9c4e02aeb
KhinYadanarNg <e0015793@u.nus.edu>
2025-09-14 16:09:47 +0800

    added workflow tracker id in rds

--- src/main/java/sg/edu/nus/iss/edgp/workflow/management/service/impl/WorkflowService.java
+++ src/main/java/sg/edu/nus/iss/edgp/workflow/management/service/impl/WorkflowService.java
@@ -91,10 +91,10 @@ public class WorkflowService implements IWorkflowService {
 		if (status != null && Status.success.toString().equals(status.toLowerCase()) && !domainTableName.isEmpty()) {
 			Map<String, Object> workflowStatusFields = dynamoItemToJavaMap(workflowStatusData);
 
-//			Optional.ofNullable(workflowStatusFields.remove("id"))
-//					.ifPresent(v -> workflowStatusFields.put("workflowStatusId", v));
+			Optional.ofNullable(workflowStatusFields.remove("id"))
+					.ifPresent(v -> workflowStatusFields.put("workflow_tracker_id", v));
 
-			Optional.ofNullable(workflowStatusFields.remove("id"));
+//			Optional.ofNullable(workflowStatusFields.remove("id"));
 			Optional.ofNullable(workflowStatusFields.remove("created_date"));
 			Optional.ofNullable(workflowStatusFields.remove("final_status"));
 			Optional.ofNullable(workflowStatusFields.remove("rule_status"));
